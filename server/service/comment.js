89c9d7f9728dd72957601006e8461ccca24326fa
Jonzheng <zhengzran@gmail.com>
2020-05-09 20:07:12 +0800

    评论save、del

--- server/service/comment.js
+++ server/service/comment.js
@@ -25,7 +25,7 @@ exports = module.exports = {
     let { recordId, commId, userId } = body
 
     await mysql('t_comment').where('id', commId).andWhere('user_id', userId).delete()
-    await mysql('t_comment').where('re_id', commId).update({ nick_name: '', re_content: ''})
+    await mysql('t_comment').where('re_id', commId).update({ re_name: '', re_content: ''})
     await mysql.raw('update t_record set comm = comm-1 where record_id = ?', recordId)
 
     let res = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,t_ur.openid,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) left join t_zan tz on (t_cm.id=tz.comm_id and tz.status=1 and tz.user_id = ?) where t_cm.record_id = ? order by t_cm.c_date desc', [userId, recordId])
