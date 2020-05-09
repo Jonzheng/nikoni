3f8c543a1c5d4197d49ec0b2eac42d4f81fca948
Jonzheng <zhengzran@gmail.com>
2020-05-09 20:02:07 +0800

    评论save、del

--- server/service/comment.js
+++ server/service/comment.js
@@ -4,8 +4,8 @@ exports = module.exports = {
   queryComment: async (ctx) => {
     let body = ctx.request.body
     let { recordId, userId } = body
-    let data = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) left join t_zan tz on (t_cm.id=tz.comm_id and tz.status=1 and tz.user_id = ?) where t_cm.record_id = ? order by t_cm.c_date desc', [userId, recordId])
-    ctx.body = data
+    let res = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,t_ur.openid,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) left join t_zan tz on (t_cm.id=tz.comm_id and tz.status=1 and tz.user_id = ?) where t_cm.record_id = ? order by t_cm.c_date desc', [userId, recordId])
+    ctx.body = res[0]
   },
   saveComment: async (ctx) => {
     let body = ctx.request.body
@@ -17,17 +17,18 @@ exports = module.exports = {
     await mysql.raw('insert t_comment (id,record_id,user_id,file_id,master_id,content,re_id,re_name,re_content) values(?,?,?,?,?,?,?,?,?)on duplicate key update status = 1', [id, recordId, userId, fileId, masterId,content,reId,reName,reContent]);
     await mysql.raw('update t_record set comm = comm+1 where record_id = ?', recordId);
 
-    let data = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) left join t_zan tz on (t_cm.id=tz.comm_id and tz.status=1 and tz.user_id = ?) where t_cm.record_id = ? order by t_cm.c_date desc', [userId, recordId])
-    ctx.body = data
+    let res = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,t_ur.openid,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) left join t_zan tz on (t_cm.id=tz.comm_id and tz.status=1 and tz.user_id = ?) where t_cm.record_id = ? order by t_cm.c_date desc', [userId, recordId])
+    ctx.body = res[0]
   },
   deleteComment: async (ctx) => {
     let body = ctx.request.body
     let { recordId, commId, userId } = body
 
-    await mysql.raw('delete from t_comment where id = ? and user_id = ?', [commId, userId]);
-    await mysql.raw('update t_record set comm = comm-1 where record_id = ?', recordId);
+    await mysql('t_comment').where('id', commId).andWhere('user_id', userId).delete()
+    await mysql('t_comment').where('re_id', commId).update({ nick_name: '', re_content: ''})
+    await mysql.raw('update t_record set comm = comm-1 where record_id = ?', recordId)
 
-    let data = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) left join t_zan tz on (t_cm.id=tz.comm_id and tz.status=1 and tz.user_id = ?) where t_cm.record_id = ? order by t_cm.c_date desc', [userId, recordId])
-    ctx.body = data
+    let res = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,t_ur.openid,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) left join t_zan tz on (t_cm.id=tz.comm_id and tz.status=1 and tz.user_id = ?) where t_cm.record_id = ? order by t_cm.c_date desc', [userId, recordId])
+    ctx.body = res[0]
   },
 }
--- server/service/detail.js
+++ server/service/detail.js
@@ -11,7 +11,7 @@ exports = module.exports = {
     data["record"] = []
     if (fileId){
         data["audio"] = await mysql('t_audio').select('*').where('file_id', fileId).andWhere('status', 1)
-        let res = await mysql.raw('select th.user_id heart_ud,t_re.*,t_ur.*,to_days(now()) - to_days(t_re.c_date) dday from t_record t_re inner join t_user t_ur on (t_re.master_id = t_ur.openid) left join t_heart th on (th.record_id = t_re.record_id and th.status = 1 and th.user_id = ?) where t_re.file_id = ? and t_re.status = 1 order by case when to_days(now()) - to_days(t_re.c_date) < 3 then 0 else 1 end,heart desc', [userId, fileId])
+        let res = await mysql.raw('select th.user_id heart_ud,t_re.*,t_ur.nick_name,t_ur.show_name,t_ur.avatar_url,t_ur.openid,to_days(now()) - to_days(t_re.c_date) dday from t_record t_re inner join t_user t_ur on (t_re.master_id = t_ur.openid) left join t_heart th on (th.record_id = t_re.record_id and th.status = 1 and th.user_id = ?) where t_re.file_id = ? and t_re.status = 1 order by case when to_days(now()) - to_days(t_re.c_date) < 3 then 0 else 1 end,heart desc', [userId, fileId])
         data["record"] = res[0]
         //ssr_yzq_0_3  ssr_h_0_0  ssr_hnj_0_0
         let fids = fileId.split("_")
