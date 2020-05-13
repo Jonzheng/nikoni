45121682892d5986a2c07f683174e4b667a1a05d
Jonzheng <zhengzran@gmail.com>
2020-05-13 21:07:35 +0800

    查询我的录音

--- server/service/record.js
+++ server/service/record.js
@@ -1,6 +1,19 @@
 const { mysql } = require('../config/db')
 
 exports = module.exports = {
+  queryRecord: async (ctx) => {
+    let body = ctx.request.body
+    let { masterId, admin } = body
+    let data = []
+    if (masterId){
+        let res = await mysql.raw('select th.user_id heart_ud, tre.*, tli.serifu, tli.title from t_record tre LEFT JOIN t_list tli on tre.file_id = tli.file_id LEFT JOIN t_heart th on tre.record_id = th.record_id where tre.master_id = ?',[masterId])
+        data = res[0]
+    }else if(admin == 'admini'){
+      let res = await mysql.raw('select th.user_id heart_ud, tre.*, tli.serifu, tli.title from t_record tre LEFT JOIN t_list tli on tre.file_id = tli.file_id LEFT JOIN t_heart th on tre.record_id = th.record_id')
+      data = res[0]
+    }
+    ctx.body = data
+  },
   saveRecord: async (ctx) => {
     let body = ctx.request.body
     let { recordId, fileId, srcRecord, masterId } = body
@@ -26,4 +39,10 @@ exports = module.exports = {
     let data = await mysql("t_record").where("record_id", recordId).update({ status: status})
     ctx.body = data
   },
+  deleteRecord: async (ctx) => {
+    let body = ctx.request.body
+    let { recordId, masterId } = body
+    await mysql("t_record").where("record_id", recordId).andWhere('master_id', masterId).delete()
+    ctx.body = 200
+  }
 }
