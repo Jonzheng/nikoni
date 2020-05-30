03c840bcc8a31a4315d239df4234603cc63bb4bd
v_vyhzheng <v_vyhzheng@tencent.com>
2020-05-30 22:18:00 +0800

    点赞状态控制

--- server/config/conf.js
+++ server/config/conf.js
@@ -2,7 +2,7 @@ const appId = 'wx2ce3e7794b9393b8'
 const appSecret = '258ce2e03d8dd6f330d6d6f3423411da'
 
 module.exports = {
-    port: 4000,
+    port: 3000,
     bucket: 'image-1256378396',
     region: 'ap-guangzhou',
 
