b327db4e36a036f2237af1b1f699095a38a68409
v_vyhzheng <v_vyhzheng@tencent.com>
2020-05-30 20:41:27 +0800

    cancel heart

--- server/service/heart.js
+++ server/service/heart.js
@@ -4,7 +4,9 @@ exports = module.exports = {
   updateHeart: async (ctx) => {
     let body = ctx.request.body
     let { recordId, userId, fileId, masterId } = body
-    await mysql.raw('insert t_heart (record_id,user_id,file_id,master_id) values(?,?,?,?)on duplicate key update status = 1', [recordId, userId, fileId, masterId]);
+    let res = await mysql.raw('insert t_heart (record_id,user_id,file_id,master_id) values(?,?,?,?)on duplicate key update status = 1 where status = 0', [recordId, userId, fileId, masterId])
+    console.log("==========res=====")
+    console.log(res)
     await mysql("t_record").where("record_id", recordId).increment({ heart: 1 })
     if(userId != masterId){
       await mysql("t_user").where("openid", masterId).increment({ news: 1 })
@@ -14,11 +16,11 @@ exports = module.exports = {
   cancelHeart: async (ctx) => {
     let body = ctx.request.body
     let { recordId, userId } = body
-    let res = await mysql("t_heart").where("record_id", recordId).andWhere("user_id", userId).andWhere("status", 1).update({ status: 0 })
-    console.log('res==================')
-    console.log(res)
-    await mysql("t_record").where("record_id", recordId).decrement({ heart: 1 })
-    ctx.body = 200
+    let count = await mysql("t_heart").where("record_id", recordId).andWhere("user_id", userId).andWhere("status", 1).update({ status: 0 })
+    if(count > 0){
+      await mysql("t_record").where("record_id", recordId).decrement({ heart: 1 })
+    }
+    ctx.body = count
   },
 
 }
