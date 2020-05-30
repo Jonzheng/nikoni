7c6a181313df72456d0c9d40ee300c280e225da5
v_vyhzheng <v_vyhzheng@tencent.com>
2020-05-30 22:05:32 +0800

    cancel heart

--- server/service/heart.js
+++ server/service/heart.js
@@ -5,16 +5,19 @@ exports = module.exports = {
     let body = ctx.request.body
     let { recordId, userId, fileId, masterId } = body
     let res = await mysql("t_heart").select('record_id').where("record_id", recordId).andWhere("user_id", userId)
-    let count = res.length
-    // console.log(res)
-    // let res = await mysql.raw('insert t_heart (record_id,user_id,file_id,master_id) values(?,?,?,?)on duplicate key update status = 1', [recordId, userId, fileId, masterId])
-    console.log("==========res=====")
-    console.log(count)
-    console.log("==========res=====")
-    // await mysql("t_record").where("record_id", recordId).increment({ heart: 1 })
-    // if(userId != masterId){
-    //   await mysql("t_user").where("openid", masterId).increment({ news: 1 })
-    // }
+    let count = 0
+    if (res.length == 1){
+      count = await mysql("t_heart").where("record_id", recordId).andWhere("user_id", userId).andWhere("status", 0).update({ status: 1})
+    }else{
+      await mysql('t_heart').insert({'record_id':recordId, 'user_id':userId, 'file_id':fileId, 'master_id':masterId})
+      count = 1
+    }
+    if (count > 0){
+      await mysql("t_record").where("record_id", recordId).increment({ heart: 1 })
+      if(userId != masterId){
+        await mysql("t_user").where("openid", masterId).increment({ news: 1 })
+      }
+    }
     ctx.body = count
   },
   cancelHeart: async (ctx) => {
