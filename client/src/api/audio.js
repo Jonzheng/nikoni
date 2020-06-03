0b80e8a67961a9d413a1e011ee88207a2bb0c9ad
v_vyhzheng <v_vyhzheng@tencent.com>
2020-06-03 19:19:00 +0800

    publish

--- client/src/api/audio.js
+++ client/src/api/audio.js
@@ -3,3 +3,7 @@ import { post } from '@/utils/axios'
 export function publishAudio(data) {
   return post('/publishAudio', data)
 }
+
+export function queryAudio(data) {
+  return post('/queryAudio', data)
+}
