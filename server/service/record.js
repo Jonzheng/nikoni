5337769c04a8c6a3cf4c7034cd3a541d3ead2d0d
v_vyhzheng <v_vyhzheng@tencent.com>
2020-07-28 21:04:23 +0800

    records 分页

--- server/service/record.js
+++ server/service/record.js
@@ -16,21 +16,25 @@ exports = module.exports = {
       pageNo = pageNo ? pageNo : 1
       pageSize = pageSize ? pageSize : 20
       let idx = (pageNo - 1) * pageSize
-      let records = await cache.get('records')
-      let reCount = await cache.get('reCount') || 0
-      let t0 = new Date().getTime()
-      console.log('type:', type(records), type(reCount))
-      if(records && reCount > 5){
-        reCount += 1
-        records = JSON.parse(records)
-        data = records.slice(idx, idx + pageSize)
-      } else {
-        let res = await mysql.raw('select th.user_id heart_ud,t_re.*, tli.serifu, tli.title,t_ur.nick_name,t_ur.show_name,t_ur.avatar_url,t_ur.openid,t_ur.cv from t_record t_re LEFT JOIN t_list tli on t_re.file_id = tli.file_id inner join t_user t_ur on (t_re.master_id = t_ur.openid) left join t_heart th on (th.record_id = t_re.record_id and th.status = 1 and th.user_id = ?) where t_re.status = 1 order by t_re.c_date desc', [openid])
-        await cache.set('records', res[0])
-        data = res[0].slice(idx, idx + pageSize)
+      // let records = await cache.get('records')
+      // let reCount = await cache.get('reCount') || 0
+      // if(records && reCount < 10){
+      //   reCount += 1
+      //   records = JSON.parse(records)
+      //   data['records'] = records.slice(idx, idx + pageSize)
+      // } else {
+      // }
+      let res = await mysql.raw('select th.user_id heart_ud,t_re.*, tli.serifu, tli.title,t_ur.nick_name,t_ur.show_name,t_ur.avatar_url,t_ur.openid,t_ur.cv from t_record t_re LEFT JOIN t_list tli on t_re.file_id = tli.file_id inner join t_user t_ur on (t_re.master_id = t_ur.openid) left join t_heart th on (th.record_id = t_re.record_id and th.status = 1 and th.user_id = ?) where t_re.status = 1 order by t_re.c_date desc', [openid])
+      // await cache.set('records', res[0])
+      data['records'] = res[0].slice(idx, idx + pageSize)
+      let avatars = []
+      for (let record of data['records']){
+        let avatar = record.avatar_url || ''
+        if (avatars.filter(it => it.url == avatar).length == 0){
+          avatars.push({ url: avatar, openid: record.openid, cv: record.cv })
+        }
       }
-      let t1 = new Date().getTime()
-      console.log(reCount, 'time:', (t1 - t0) / 1000)
+      data['avatars'] = avatars
     }else if(admin == 'admini' && masterId){ // 小程序无引用
       let res = await mysql.raw('select th.user_id heart_ud, tre.*, tli.serifu, tli.title from t_record tre LEFT JOIN t_list tli on tre.file_id = tli.file_id LEFT JOIN t_heart th on tre.record_id = th.record_id and th.status = 1 and th.user_id = ? ORDER BY tre.c_date desc',[masterId])
       data = res[0].slice(idx, idx + pageSize)
