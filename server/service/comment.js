e9f6178404492a42535b589703b8ae2924a83718
v_vyhzheng <v_vyhzheng@tencent.com>
2021-02-20 14:38:24 +0800

    chat-dev

--- server/service/comment.js
+++ server/service/comment.js
@@ -73,7 +73,7 @@ exports = module.exports = {
     let id = fileId + new Date().getTime()
     await mysql.raw('insert t_comment (id,record_id,user_id,file_id,master_id,content,re_id,re_name,re_content) values(?,?,?,?,?,?,?,?,?)on duplicate key update status = 1', [id, recordId, userId, fileId, masterId,content,reId,reName,reContent]);
     if(userId != masterId){
-      await mysql("t_user").where("openid", masterId).increment({ news: 1 })
+      await mysql("t_user").where("openid", userId).increment({ news: 1 })
     }
     let res = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,t_ur.openid,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) left join t_zan tz on (t_cm.id=tz.comm_id and tz.status=1 and tz.user_id = ?) where t_cm.record_id = ? order by t_cm.c_date desc', [userId, recordId])
     let comments = res[0]
