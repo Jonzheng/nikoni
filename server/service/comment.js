a43cebf809825aa10bf7702ec3b23d0ed7aa96b8
v_vyhzheng <v_vyhzheng@tencent.com>
2021-02-20 11:22:58 +0800

    chat-dev

--- server/service/comment.js
+++ server/service/comment.js
@@ -56,4 +56,35 @@ exports = module.exports = {
     }
     ctx.body = comments
   },
+  queryChat: async (ctx) => {
+    let body = ctx.request.body
+    let { recordId, userId, masterId } = body
+    recordId = 'chat'
+    let res = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,t_ur.openid,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) where t_cm.record_id = ? and t_cm.user_id = ? and t_cm.master_id = ?', [recordId, userId, masterId])
+    let comments = res[0]
+    ctx.body = comments
+  },
+  saveChat: async (ctx) => {
+    let body = ctx.request.body
+    let { recordId, userId, fileId, masterId, content, reId, reName, reContent } = body
+    reId = reId ? reId : ''
+    reName = reName ? reName : ''
+    reContent = reContent ?  reContent : ''
+    let id = fileId + new Date().getTime()
+    await mysql.raw('insert t_comment (id,record_id,user_id,file_id,master_id,content,re_id,re_name,re_content) values(?,?,?,?,?,?,?,?,?)on duplicate key update status = 1', [id, recordId, userId, fileId, masterId,content,reId,reName,reContent]);
+    if(userId != masterId){
+      await mysql("t_user").where("openid", masterId).increment({ news: 1 })
+    }
+    let res = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,t_ur.openid,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) left join t_zan tz on (t_cm.id=tz.comm_id and tz.status=1 and tz.user_id = ?) where t_cm.record_id = ? order by t_cm.c_date desc', [userId, recordId])
+    let comments = res[0]
+    ctx.body = comments
+  },
+  deleteMessage: async (ctx) => {
+    let body = ctx.request.body
+    let { recordId, commId, userId } = body
+    await mysql('t_comment').where('id', commId).andWhere('user_id', userId).delete()
+    let res = await mysql.raw('select t_cm.*,t_ur.show_name,t_ur.nick_name,t_ur.avatar_url,t_ur.openid,tz.user_id as zid from t_comment t_cm inner join t_user t_ur on (t_cm.user_id = t_ur.openid) left join t_zan tz on (t_cm.id=tz.comm_id and tz.status=1 and tz.user_id = ?) where t_cm.record_id = ? order by t_cm.c_date desc', [userId, recordId])
+    let comments = res[0]
+    ctx.body = comments
+  },
 }
